2d5fcc15d9529b3d0ada5b9415487b04fd749fa5
unknown <rkeegan@live.com>
2022-09-15 13:49:12 -0400

    Changed anchor tags to Next/Link

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { MdArrowBack } from "react-icons/md";
 import styles from "../styles/Home.module.css";
@@ -6,11 +7,11 @@ const BackButton = () => {
     return (
         <div id={styles.iconButton} className="flex flex-col h-full items-center justify-center px-5">
             <div className="flex flex-col items-center rounded-lg justify-center p-0.5 bg-gradient-to-r from-purple-400 to-rose-400 hover:shadow-[-1px_1px_5px] hover:shadow-rose-300">
-                <a href="/">
+                <Link href="/">
                   <button className="flex items-center justify-center rounded-md p-0.5 bg-gray-900">
                     <MdArrowBack size={30}/>
                   </button>
-                </a>
+                </Link>
             </div>
         </div>
     );
--- components/ImageDownloadButton.tsx
+++ components/ImageDownloadButton.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { MdOutlineFileDownload } from 'react-icons/md';
 import styles from "../styles/Home.module.css";
@@ -6,11 +7,11 @@ const ImageDownloadButton = () => {
     return (
       <div id={styles.iconButton} className="flex flex-col h-full items-center justify-center px-10">
         <div className="flex flex-col items-center rounded-lg justify-center p-1 bg-gradient-to-r from-purple-500 to-rose-400 hover:shadow-[-1px_1px_5px] hover:shadow-rose-300">
-            <a href="/api/ImageDownload" download>
+            <Link href="/api/ImageDownload" download>
               <button className="flex items-center justify-center rounded-md p-1 bg-gray-900">
                 <MdOutlineFileDownload size={30} />
               </button>
-            </a>
+            </Link>
         </div>
       </div>
     );
